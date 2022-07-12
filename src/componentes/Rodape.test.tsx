6f2e4cf401778a916f9c61f3d959d3b501248488
thabataganga <ganga.thabata@gmail.com>
2022-07-11 23:06:33 -0300

    navega para sorteio

--- src/componentes/Rodape.test.tsx
+++ src/componentes/Rodape.test.tsx
@@ -1,9 +1,28 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import React from "react"
 import { RecoilRoot } from "recoil"
 import Rodape from "./Rodape"
+import { useListaDeParticipantes } from "../state/hook/useListaDeParticipante"
+
+
+jest.mock("../state/hook/useListaDeParticipante", () => {
+  return {
+    useListaDeParticipantes: jest.fn()
+  }
+})
+
+const mockNavegacao = jest.fn()
+
+jest.mock("react-router-dom", () => {
+  return {
+    useNavigate: () => mockNavegacao
+  }
+})
 
 describe("participantes insuficientes", () => {
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue([])
+  })
   test("não pode ser iniciado", () => {
     render(
       <RecoilRoot>
@@ -13,5 +32,37 @@ describe("participantes insuficientes", () => {
     const botao = screen.getByRole('button')
 
     expect(botao).toBeDisabled()
+  })
+})
+
+describe("participantes suficientes", () => {
+  const participantes = ["Ana", "Catarina", "Sheila"]
+  beforeEach(() => {
+    (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
+  })
+
+  test("pode ser iniciado", () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>)
+
+    const botao = screen.getByRole('button')
+
+    expect(botao).not.toBeDisabled()
+  })
+
+  test("inicia sorteio", () => {
+    render(
+      <RecoilRoot>
+        <Rodape />
+      </RecoilRoot>)
+
+    const botao = screen.getByRole('button')
+    fireEvent.click(botao)
+
+    expect(mockNavegacao).toHaveBeenCalledTimes(1)
+    expect(mockNavegacao).toHaveBeenCalledWith("/sorteio")
+
   })
 })
--- src/componentes/Rodape.tsx
+++ src/componentes/Rodape.tsx
@@ -3,10 +3,15 @@ import { useListaDeParticipantes } from "../state/hook/useListaDeParticipante"
 
 export default function Rodape() {
   const participantes = useListaDeParticipantes()
-  //const navegarPara = useNavigate()
+  const navegarPara = useNavigate()
+
+  function iniciar() {
+    navegarPara('/sorteio')
+  }
+
   return (
     <footer>
-      <button disabled={participantes.length < 3}>RODAR</button>
+      <button onClick={iniciar} disabled={participantes.length < 3}>RODAR</button>
     </footer>
   )
 };
